a3b6a860d085147dfa55e5bb0186aff62d562f84
lokesh7791 <bobbadi.lokesh077@gmail.com>
2024-07-22 15:48:21 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# echo "updating system"
-# sudo apt update -y
-#
-# echo "Installing Utilities"
-# sudo apt insatll -y zip unzip
-#
-# echo "Installing NGINX web Server"
-# sudo apt install -y nginx
-#
-# echo "Remove Sample Pages"
-# sudo rm -rf /var/www/html
-#
-# echo "Clone Login App"
-# sudo git clone https://github.com/blnaidu/login-project.git
-#
-# echo"Script Execution Completed"
+echo "updating system"
+sudo apt update -y
+
+echo "Installing Utilities"
+sudo apt insatll -y zip unzip
+
+echo "Installing NGINX web Server"
+sudo apt install -y nginx
+
+echo "Remove Sample Pages"
+sudo rm -rf /var/www/html
+
+echo "Clone Login App"
+sudo git clone https://github.com/blnaidu/login-project.git
+
+echo"Script Execution Completed"
 #
 #
 #
